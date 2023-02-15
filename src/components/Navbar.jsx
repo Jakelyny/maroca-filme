61df3515a1b76a383dfa5ffa158ba87192efdea6
Jakelyny Monroe <85123013+Jakelyny@users.noreply.github.com>
2023-02-14 23:41:53 -0300

    Adjusting the search and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,40 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiMoviePlay, BiSearchAlt2 } from "react-icons/bi";
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
   return (
-    <nav id='navbar'>
-        <h2>
-            <Link to='/'> <BiMoviePlay size={35}/> Maroca Filme</Link>
-        </h2>
-        <form>
-            <input type="text" placeholder="Buscar" />
-            <button type="submit">
-                <BiSearchAlt2 />
-            </button>
-        </form>
+    <nav id="navbar">
+      <h2>
+      <Link to='/'> <BiMoviePlay size={35}/> Maroca Filme</Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
